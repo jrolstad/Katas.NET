2ab2f14039e519d6a93c544aec0b7053761e45bc
Josh Rolstad <jrolstad@gmail.com>
2012-06-07 21:20:06 -0700

    Refactoring for clarity

--- StringCalculator/Implementation/Calculator.cs
+++ StringCalculator/Implementation/Calculator.cs
@@ -7,55 +7,88 @@ namespace StringCalculator.Implementation
 {
     public class Calculator
     {
+        const string delimiterLineIndicator = "//";
+
          public int Add(string stringNumbers)
          {
              // If something we can't parse, then return zero
              if (string.IsNullOrWhiteSpace(stringNumbers)) 
                  return 0;
 
-             const char newLineSeperator = '\n';
-             var lines = stringNumbers.Split(newLineSeperator);
+             // Seperate into lines
+             var lines = ParseLines(stringNumbers);
 
-             // Known set of delimiters
-             var delimiters = new List<string>{ ","};
+             // Get the delimiters
+             var allowedDelimiters = new List<string> { "," };
+             var userDefinedDelimiters = GetUserDefinedDelimiters(lines);
+             allowedDelimiters.AddRange(userDefinedDelimiters);
 
-             // Get the delimiter
-             const string delimiterLineIndicator = "//";
-             if(lines[0].StartsWith(delimiterLineIndicator))
-             {
-                 var delimiter = lines[0]
-                     .Replace("//", "")
-                     .Replace("[", "");
-                 var definedDelimiters = delimiter.Split(']');
-
-                 delimiters.AddRange(definedDelimiters);
-             }
-
-             // Summarize them
-             var parsedNumbers = lines
-                 .Where(line => !line.StartsWith(delimiterLineIndicator))
-                 .SelectMany(line => line.Split(delimiters.ToArray(),StringSplitOptions.RemoveEmptyEntries))
-                 .Where(s => !string.IsNullOrWhiteSpace(s))
-                 .Select(s => Int32.Parse(s.Trim()))
-                 .ToArray();
+             // Convert the numbers
+             var parsedNumbers = ParseNumbers(allowedDelimiters, lines);
 
              // Validate for negative numbers
-             var negativeNumbers = parsedNumbers
-                 .Where(number => number < 0)
-                 .ToArray();
+             Validate(parsedNumbers);
 
-             if (negativeNumbers.Any())
-             {
-                 var numbers = string.Join(",", negativeNumbers);
-                 var message = "Only positive numbers can be added. Negative values are: {0}".StringFormat(numbers);
-                 throw new ArgumentOutOfRangeException("stringNumbers", message);
-             }
-
-             var sum = parsedNumbers
-                 .Where(n=> n<=1000)
-                 .Sum();       
+             // Sum them
+             var sum = SummarizeNumbers(parsedNumbers);       
 
              return sum;
          }
+
+        private static int SummarizeNumbers(int[] parsedNumbers)
+        {
+            const int upperBound = 1000;
+            return parsedNumbers
+                .Where(n=> n<=upperBound)
+                .Sum();
+        }
+
+        private static string[] ParseLines(string stringNumbers)
+        {
+            const char newLineSeperator = '\n';
+            var lines = stringNumbers.Split(newLineSeperator);
+            return lines;
+        }
+
+        private static void Validate(int[] parsedNumbers)
+        {
+            var negativeNumbers = parsedNumbers
+                .Where(number => number < 0)
+                .ToArray();
+
+            if (negativeNumbers.Any())
+            {
+                var numbers = string.Join(",", negativeNumbers);
+                var message = "Only positive numbers can be added. Negative values are: {0}".StringFormat(numbers);
+                throw new ArgumentOutOfRangeException("parsedNumbers", message);
+            }
+        }
+
+        private static int[] ParseNumbers(List<string> delimiters, string[] lines)
+        {
+            return lines
+                .Where(line => !line.StartsWith(delimiterLineIndicator))
+                .SelectMany(line => line.Split(delimiters.ToArray(),StringSplitOptions.RemoveEmptyEntries))
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => Int32.Parse(s.Trim()))
+                .ToArray();
+        }
+
+        private static IEnumerable<string> GetUserDefinedDelimiters(string[] lines)
+        {
+            var delimiters = new List<string>();
+
+            if (lines[0].StartsWith(delimiterLineIndicator))
+            {
+                var delimiter = lines[0]
+                    .Replace(delimiterLineIndicator, "")
+                    .Replace("[", "");
+                var definedDelimiters = delimiter.Split(']');
+
+                delimiters.AddRange(definedDelimiters);
+            }
+
+            return delimiters;
+        }
     }
 }
